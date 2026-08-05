41cdfb129f93b4fdfd387bdda62b2886ace5f41e
MederbekTuratbekov <142796921+MederbekTuratbekov@users.noreply.github.com>
2026-08-05 15:46:00 +0600

    ...

--- StudentsPerformance/main.py
+++ StudentsPerformance/main.py
@@ -1,6 +1,9 @@
+# StudentsPerformance/main.py
+
 from pydantic import BaseModel
 from fastapi import FastAPI
 from pathlib import Path
+import pandas as pd
 import uvicorn
 import joblib
 
@@ -11,6 +14,21 @@ model = joblib.load(BASE_DIR / 'model_StudentsPerformance.pkl')
 scaler = joblib.load(BASE_DIR / 'scaler_StudentsPerformance.pkl')
 
 student_app = FastAPI()
+
+FEATURE_ORDER = [
+    'math score', 'reading score', 'avarage_score', '3 subjects',
+    'gender_male',
+    'race/ethnicity_group B', 'race/ethnicity_group C',
+    'race/ethnicity_group D', 'race/ethnicity_group E',
+    "parental level of education_bachelor's degree",
+    'parental level of education_high school',
+    "parental level of education_master's degree",
+    'parental level of education_some college',
+    'parental level of education_some high school',
+    'lunch_standard',
+    'test preparation course_none',
+]
+
 
 class Student(BaseModel):
     gender: str
@@ -24,50 +42,37 @@ class Student(BaseModel):
 
 @student_app.post('/predict/')
 async def check_score(student: Student):
-    student_dict = dict(student)
-
-    math_score = student_dict['math_score']
-    reading_score = student_dict['reading_score']
+    math_score = student.math_score
+    reading_score = student.reading_score
     avarage_score = round((math_score + reading_score) / 2, 2)
     three_subjects = math_score + reading_score
 
-    new_gender = student_dict.pop('gender')
-    gender_binar = [1 if new_gender == 'male' else 0]
+    row = {
+        'math score': math_score,
+        'reading score': reading_score,
+        'avarage_score': avarage_score,
+        '3 subjects': three_subjects,
+        'gender_male': 1 if student.gender == 'male' else 0,
+        'race/ethnicity_group B': 1 if student.race_ethnicity == 'group B' else 0,
+        'race/ethnicity_group C': 1 if student.race_ethnicity == 'group C' else 0,
+        'race/ethnicity_group D': 1 if student.race_ethnicity == 'group D' else 0,
+        'race/ethnicity_group E': 1 if student.race_ethnicity == 'group E' else 0,
+        "parental level of education_bachelor's degree": 1 if student.parent == "bachelor's degree" else 0,
+        'parental level of education_high school': 1 if student.parent == 'high school' else 0,
+        "parental level of education_master's degree": 1 if student.parent == "master's degree" else 0,
+        'parental level of education_some college': 1 if student.parent == 'some college' else 0,
+        'parental level of education_some high school': 1 if student.parent == 'some high school' else 0,
+        'lunch_standard': 1 if student.lunch == 'standard' else 0,
+        'test preparation course_none': 1 if student.test == 'none' else 0,
+    }
 
-    new_race_ethnicity = student_dict.pop('race_ethnicity')
-    race_ethnicity_binar = [
-        1 if new_race_ethnicity == 'group B' else 0,
-        1 if new_race_ethnicity == 'group C' else 0,
-        1 if new_race_ethnicity == 'group D' else 0,
-        1 if new_race_ethnicity == 'group E' else 0,
-    ]
+    features_df = pd.DataFrame([row])[FEATURE_ORDER]
 
-    new_parent = student_dict.pop('parent')
-    parent_binar = [
-        1 if new_parent == "bachelor's degree" else 0,
-        1 if new_parent == 'high school' else 0,
-        1 if new_parent == "master's degree" else 0,
-        1 if new_parent == 'some college' else 0,
-        1 if new_parent == 'some high school' else 0,
-    ]
-
-    new_lunch = student_dict.pop('lunch')
-    lunch_binar = [1 if new_lunch == 'standard' else 0]
-
-    new_test = student_dict.pop('test')
-    test_binar = [1 if new_test == 'none' else 0]
-
-    features = (
-        [math_score, reading_score, avarage_score, three_subjects]
-        + gender_binar
-        + race_ethnicity_binar
-        + parent_binar
-        + lunch_binar
-        + test_binar
-    )
-    scaled = scaler.transform([features])
+    scaled = scaler.transform(features_df)
     predict = model.predict(scaled)[0]
+
     return {'Прогнозируемый бал по writing score': round(predict, 2)}
+
 
 if __name__ == '__main__':
     uvicorn.run(student_app, host='127.0.0.1', port=8000)
